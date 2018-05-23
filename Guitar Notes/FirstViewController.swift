3e5a8e4dc20c5b0cb4a88683f3672f09e5e34688
brauliojde <bjdelato@ucsc.edu>
2018-05-23 17:01:52 +0200

    Initial Commit

---
+++ Guitar Notes/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Guitar Notes
+//
+//  Created by Braulio De La Torre on 5/23/18.
+//  Copyright © 2018 Braulio. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
