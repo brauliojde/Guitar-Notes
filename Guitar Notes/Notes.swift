3468833b2e25075438126ea8d4ae68d3b45c0aba
brauliojde <bjdelato@ucsc.edu>
2018-05-30 10:14:42 +0200

    first commit

---
+++ Guitar Notes/Notes.swift
@@ -0,0 +1,9 @@
+//
+//  Notes.swift
+//  Guitar Notes
+//
+//  Created by Braulio De La Torre on 5/24/18.
+//  Copyright © 2018 Braulio. All rights reserved.
+//
+
+import Foundation
---
+++ Guitar Notes/Tuner.swift
@@ -0,0 +1,9 @@
+//
+//  Tuner.swift
+//  Guitar Notes
+//
+//  Created by Braulio De La Torre on 5/23/18.
+//  Copyright © 2018 Braulio. All rights reserved.
+//
+
+import Foundation
